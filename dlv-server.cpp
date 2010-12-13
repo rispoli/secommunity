64e98691e3e3f884fac7d969ccbee9b571aefacc
Daniele Rispoli <daniele.rispoli@gmail.com>
2010-12-13 15:11:00 +0100

    new file:   AUTHORS 	new file:   COPYING 	modified:   dlv-server.cpp 	modified:   message.h 		AGPLv3 	modified:   README 		typo

--- dlv-server.cpp
+++ dlv-server.cpp
@@ -1,3 +1,24 @@
+/*
+
+   Copyright 2010 Daniele Rispoli, Valerio Genovese, Steve Barker
+
+   This file is part of dlv-server.
+
+   dlv-server is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   dlv-server is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public
+   License along with dlv-server.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include <argtable2.h>
 #include <errno.h>
 #include <fstream>
